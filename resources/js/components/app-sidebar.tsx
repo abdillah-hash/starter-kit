666daeb8895d1af1747ef85131786d0fdde49674
abdillah-hash <abdi.lllah126@gmail.com>
2025-02-25 15:52:48 +0700

    add sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,15 +4,33 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, Layers, Clipboard, LayoutDashboard, Megaphone } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
         url: '/dashboard',
-        icon: LayoutGrid,
+        icon: LayoutDashboard,
     },
+
+    {
+        title: 'My Course',
+        url: '/my-course',
+        icon: Layers,
+    },
+
+    {
+        title: 'Attendance',
+        url: '/attendance',
+        icon: Clipboard,
+    },
+    {
+        title: 'Annoucement',
+        url: '/annoucement',
+        icon: Megaphone,
+    },
+
 ];
 
 const footerNavItems: NavItem[] = [
