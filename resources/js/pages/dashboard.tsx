7141deba3a9fd310246bbe6a3633262ca7a1da09
abdillah-hash <abdi.lllah126@gmail.com>
2025-02-25 17:11:14 +0700

    create content dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,10 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import gambar from '../../assets/image.png';
+import { GraduationCap } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,18 +19,26 @@ export default function Dashboard() {
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                </div>
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <Card className='overflow-hidden rounded-2xl'>
+                        <img src={gambar} className='object-contain'></img>
+                        <CardHeader>
+                            Nama Matkul
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className='font-bold'>
+                                Pertemuan 1
+                            </CardTitle>
+                            <CardDescription className='flex items-center gap-2'>
+                                <GraduationCap size={20} color='#ff782d'/>
+                                156 Students
+                            </CardDescription>
+                        </CardContent>
+                            <hr className='mx-6'></hr>
+                        <CardFooter className='justify-end font-medium py-4 text-lg'>
+
+                            View More
+                        </CardFooter>
+                    </Card>
                 </div>
             </div>
         </AppLayout>
